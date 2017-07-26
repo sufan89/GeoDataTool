70dc1b848d5d610b97bd3801e40aa97d960615e8
yangguichao <yanggc@idreamtech.com.cn>
2017-07-26 17:28:09 +0800

    完成ShapFile的读取和导入

---
+++ src/config.py
@@ -0,0 +1,30 @@
+# coding=UTF-8
+
+
+
+class importdatatype:
+    def __init__(self):
+        pass
+    # shapefile单个文件
+    SHAPEFILE=1
+    # GDB数据库
+    FILEGEODATABASE=2
+    # MDB数据库
+    PERSONALGEODATABASE=3
+    # shapfile文件夹
+    SHAPEFILEPATH=4
+    # 栅格数据
+    RASTER=5
+    # 其他数据格式
+    OTHER=0
+
+class importOperate:
+    INSERT=1
+    CREATR=2
+    UPDATE=3
+
+class config:
+    DATABASE_URL = "postgresql://postgres:sufan2008300379@localhost/dbgeoserver"
+    Enum = importdatatype()
+    IMPORT_DATA_TYPE = Enum.SHAPEFILEPATH
+    IMPORTFILENAME = 'E:\\test\\data_shp'
